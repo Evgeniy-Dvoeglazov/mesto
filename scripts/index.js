3d86de20ce5050fc467229373166ecd70b108873
Evgeniy Dvoeglazov <jindv@yandex.ru>
2022-12-04 17:55:07 +0300

    Проект Место. 1 Часть. Отправить на первое ревью.

---
+++ scripts/index.js
@@ -0,0 +1,31 @@
+let profileEditBtn = document.querySelector('.profile__edit-btn');
+let popup = document.querySelector('.popup');
+let editForm = document.querySelector('.popup__form');
+let closeFormBtn = document.querySelector('.popup__close-btn');
+let profileName = document.querySelector('.profile__title-text');
+let profileInfo = document.querySelector('.profile__subtitle');
+let inputName = document.querySelector('.popup__text_name');
+let inputInfo = document.querySelector('.popup__text_info');
+
+function openForm() {
+  popup.classList.add('popup_opened');
+  inputName.value = profileName.textContent;
+  inputInfo.value = profileInfo.textContent;
+}
+
+profileEditBtn.addEventListener('click', openForm);
+
+function addInformation(evt) {
+  evt.preventDefault();
+  profileName.textContent = inputName.value;
+  profileInfo.textContent = inputInfo.value;
+  popup.classList.remove('popup_opened');
+}
+
+editForm.addEventListener('submit', addInformation);
+
+function closeForm() {
+  popup.classList.remove('popup_opened');
+}
+
+closeFormBtn.addEventListener('click', closeForm);
